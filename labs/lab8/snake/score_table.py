de8fee7f7e5404a5e777851df95598422fc717cd
yantay0 <madinayantai2464@gmail.com>
2022-04-29 19:57:22 +0600

    lab10

--- labs/lab8/snake/score_table.py
+++ labs/lab8/snake/score_table.py
@@ -1,22 +1,23 @@
-import psycopg2 
+import psycopg2
 
 config = psycopg2.connect(
- host = 'localhost',
- database = 'postgres',
- user = 'postgres',
- password = 'madina2626'
+    host='localhost',
+    database='postgres',
+    user='postgres',
+    password='madina2626'
 )
 
 
 current = config.cursor()
 #current.execute("UPDATE Phonebook SET id INTEGER PRIMARY KEY")
 current.execute(
- 
-'''
+
+    '''
 CREATE TABLE Snake(
- id INTEGER PRIMARY KEY,
  user_name VARCHAR(50),
- score INTEGER
+ score INTEGER,
+ level INTEGER,
+ date VARCHAR(12)
 );
 ''')
 
